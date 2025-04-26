a73eb1065953d8c3b1bd422c290669bd7adc0274
potatoluvr <134911154+potatoluvr@users.noreply.github.com>
2025-04-25 23:33:52 -0500

    adding admin role

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,37 @@ import "./components/FooterStyle.css";
 import "./components/HeaderStyle.css";
 import VolunteerHisoryPage from "./webpages/VolunteerHisoryPage";
 import UserProfile from "./webpages/UserProfile";
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [role, setRole] = useState("");
+
+  useEffect(() => {
+    const storedRole = localStorage.getItem("role");
+    setRole(storedRole);
+  }, []);
+
+  if (localStorage.getItem("token") && role === "") {
+    return <p>Loading...</p>; // only if logged in but role not ready
+  }
+
+
   return (
     <Router>
+      <Header role={role} />  {/* Pass role to Header */}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/user-profile/edit" element={<UserProfileManagement />} />
         <Route path="/user-profile" element={<UserProfile />} />
-        <Route path="/event-management" element={<EventManagement />} />
         <Route
-          path="/user-profile/volunteer-history"
-          element={<VolunteerHisoryPage />}
+          path="/event-management"
+          element={
+            role === "admin" ? <EventManagement /> : <Navigate to="/" replace />
+          }
         />
-
+        <Route path="/user-profile/volunteer-history" element={<VolunteerHisoryPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
